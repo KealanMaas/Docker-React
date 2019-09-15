27d5ebc66052ff35fcfd4e2af5c605b5d2d3795a
KealanMaas <kealan.maas@datadoghq.com>
2019-09-15 18:32:55 +0100

    Changed App Text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Magic
+          I'm alive!
         </p>
         <a
           className="App-link"
